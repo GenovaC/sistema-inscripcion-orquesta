29f23f77e7a818e6bf6d6757389c5eb3765d5f63
Génova Castillo <genovacastillo@gmail.com>
2025-07-23 18:03:49 -0400

    fix: Contabilizar estudiantes activos por periodo academico

--- instruments/views.py
+++ instruments/views.py
@@ -3,10 +3,19 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from .forms import InstrumentForm
 from .models import Instrument
+from django.db.models import Count, Q
+  
 
 @login_required
 def list(request):
-    instruments = Instrument.objects.all()
+   # instruments = Instrument.objects.all()
+
+    instruments = Instrument.objects.annotate(
+        inscripciones_activas=Count(
+            'detailacademicinscription',
+            filter=Q(detailacademicinscription__id_academic_period__is_active=True)
+        )
+    )
     
     if request.method == 'GET':
         return render(request, 'instruments/instruments_list.html', {
--- orchestral_projects/views.py
+++ orchestral_projects/views.py
@@ -3,11 +3,19 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from .forms import OrchestralProjectForm
 from .models import OrchestralProject
+from django.db.models import Count, Q
 
 
 @login_required
 def list(request):
-    projects = OrchestralProject.objects.all()
+    # projects = OrchestralProject.objects.all()
+
+    projects = OrchestralProject.objects.annotate(
+        inscripciones_activas=Count(
+            'detailacademicinscription',
+            filter=Q(detailacademicinscription__id_academic_period__is_active=True)
+        )
+    )
     
     if request.method == 'GET':
         return render(request, 'orchestral_projects/orchestral_projects_list.html', {
